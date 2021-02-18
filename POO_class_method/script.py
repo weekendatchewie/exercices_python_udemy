1da4046802037be0b464f97ace02348ec4277aa2
chris <christopher.poncelet51@gmail.com>
2021-02-18 18:44:05 +0100

    add method

--- POO_class_method/script.py
+++ POO_class_method/script.py
@@ -1,5 +1,8 @@
 class Voiture:
+    voiture_crees = 0
+
     def __init__(self, marque, vitesse, prix):
+        Voiture.voiture_crees += 1
         self.marque = marque
         self.vitesse = vitesse
         self.prix = prix
@@ -16,7 +19,15 @@ class Voiture:
     def ford(cls):
         return cls(marque="Ford", vitesse=30, prix=160)
 
+    @staticmethod
+    def afficher_voiture():
+        if Voiture.voiture_crees <= 1:
+            print(f"Il y a {Voiture.voiture_crees} voiture")
+        else:
+            print(f"Il y a {Voiture.voiture_crees} voitures")
+
 
 # On utilise les méthode pour créer des instances plutôt que de toujours réécrire les paramètres
 lambo = Voiture.lamborghini()
 porsche = Voiture.porsche()
+Voiture.afficher_voiture()
